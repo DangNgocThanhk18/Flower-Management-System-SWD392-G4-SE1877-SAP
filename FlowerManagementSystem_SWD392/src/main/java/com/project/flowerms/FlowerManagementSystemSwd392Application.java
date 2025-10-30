eba3c6e09d66a9983036ba54febd31ab7e1cf3bf
DangNgocThanhk18 <thanhdnhe186611@fpt.edu.vn>
2025-10-30 23:38:45 +0700

    create

---
+++ FlowerManagementSystem_SWD392/src/main/java/com/project/flowerms/FlowerManagementSystemSwd392Application.java
@@ -0,0 +1,13 @@
+package com.project.flowerms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FlowerManagementSystemSwd392Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FlowerManagementSystemSwd392Application.class, args);
+    }
+
+}
---
+++ FlowerManagementSystem_SWD392/src/test/java/com/project/flowerms/FlowerManagementSystemSwd392ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.flowerms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FlowerManagementSystemSwd392ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
